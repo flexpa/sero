38d9eab5cbdec3d9e469f0f685d943f6dcea7052
Joshua Kelly <jdjkelly@gmail.com>
2021-07-09 19:20:11 -0400

    Documentation
    Improved docs

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,6 +1,20 @@
 /**
  * @module client
- * @remarks Almost no-dependency TS FHIR client (we use cross fetch to give us a fetch instance)
+ * @beta
+ * 
+ * A Fetch-ful FHIR Client in TypeScript
+ * 
+ * @example Grab a capabilities statement:
+ * ```typescript
+ *  const { capabilities } = Client("https://r4.smarthealthit.org", {});
+ *  await capabilities().json() as fhir4.CapabilityStatement;
+ * ```
+ * 
+ * @example Easily read a Patient resource:
+ * ```typescript
+ *  const { read } = Client("https://r4.smarthealthit.org", {})
+ *  await read("Patient", "87a339d0-8cae-418e-89c7-8651e6aab3c6").json() as fhir4.Patient;
+ * ```
  */
 
 import fetch from "cross-fetch";
@@ -8,6 +22,9 @@ import fetch from "cross-fetch";
 type Summary = "true" | "false" | "text" | "count" | "data";
 
 export default function(baseUrl: string, init: RequestInit) {
+  /**
+   * Read the current state of the resource
+   */
   async function read(type: string, id: string, summary?: Summary) {
     return fetch(`${baseUrl}/${type}/${id}`, {
       method: "GET",
@@ -15,6 +32,9 @@ export default function(baseUrl: string, init: RequestInit) {
     });
   }
 
+  /**
+   * Read the state of a specific version of the resource
+   */
   async function vread(type: string, id: string, vid: string) {
     return fetch(`${baseUrl}/${type}/${id}/_history/${vid}`, {
       method: "GET",
@@ -22,6 +42,9 @@ export default function(baseUrl: string, init: RequestInit) {
     });
   }
   
+  /**
+   * Update an existing resource by its id (or create it if it is new)
+   */
   async function update(type: string, id: string, resource: fhir3.FhirResource | fhir4.FhirResource) {
     return fetch(`${baseUrl}/${type}/${id}`, {
       method: "PUT",
@@ -30,6 +53,9 @@ export default function(baseUrl: string, init: RequestInit) {
     });
   }
 
+  /**
+   * Update an existing resource by posting a set of changes to it
+   */
   async function patch(type: string, id: string, resource: fhir3.FhirResource | fhir4.FhirResource) {
     return fetch(`${baseUrl}/${type}/${id}`, {
       method: "PATCH",
@@ -38,6 +64,9 @@ export default function(baseUrl: string, init: RequestInit) {
     });
   }
 
+  /**
+   * Delete a resource
+   */
   async function destroy(type: string, id: string) {
     return fetch(`${baseUrl}/${type}/${id}`, {
       method: "DELETE",
@@ -46,6 +75,7 @@ export default function(baseUrl: string, init: RequestInit) {
   }
 
   /**
+   * Search the resource type based on some filter criteria OR Search across all resource types based on some filter criteria
    * @todo not complete
    */
   async function search(type: string) {
@@ -55,6 +85,9 @@ export default function(baseUrl: string, init: RequestInit) {
     });
   }
 
+  /**
+   * Create a new resource with a server assigned id
+   */
   async function create(type: string, id: string) {
     return fetch(`${baseUrl}/${type}`, {
       method: "POST",
@@ -62,10 +95,16 @@ export default function(baseUrl: string, init: RequestInit) {
     });
   }
 
+  /**
+   * Retrieve the change history for a particular resource type OR Retrieve the change history for all resources
+   */
   async function history() {
     throw new Error("Not Implemented");
   }
 
+  /**
+   * Get a capability statement for the system
+   */
   async function capabilities() {
     return fetch(`${baseUrl}/metadata`, {
       method: "GET",
@@ -73,6 +112,9 @@ export default function(baseUrl: string, init: RequestInit) {
     });
   }
 
+  /**
+   * Update, create or delete a set of resources in a single interaction
+   */
   async function batch(bundle: fhir3.Bundle | fhir4.Bundle) {
     return fetch(`${baseUrl}/`, {
       method: "POST",
@@ -81,6 +123,9 @@ export default function(baseUrl: string, init: RequestInit) {
     });
   }
 
+  /**
+   * Update, create or delete a set of resources in a single interaction
+   */
   async function transaction(bundle: fhir3.Bundle | fhir4.Bundle) {
     return fetch(`${baseUrl}/`, {
       method: "POST",
