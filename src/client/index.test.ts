fc1c6f05c236dbdc04b75f866afed5532dbab287
Joshua Kelly <jdjkelly@gmail.com>
2021-07-09 22:06:19 -0400

    FHIR Client (#18)
    * FHIR Client
    
    I wrote a FHIR client that is a simple wrapper about fetch.
    
    This is helpful because it's a minimal library that can work in any JavaScript environment (getting a cross-platform/isomorphic HTTP client API working is actually kind of hard tbh)
    
    Anyways, this turned out to be way simpler than expected and includes some real production tests
    
    It, sadly, does add one production dependency

---
+++ src/client/index.test.ts
@@ -0,0 +1,29 @@
+import Client from ".";
+
+test('Calling Capabilities returns a 200', async () => {
+  // @todo this test should use a mock, not the live 
+  const { capabilities } = Client("https://r4.smarthealthit.org", {})
+
+  const statement = await capabilities()
+  const body = await statement.json();
+
+  expect(statement.status).toEqual(200)
+  expect(body.resourceType).toEqual("CapabilityStatement")
+});
+
+test('Calling Patient query returns some', async () => {
+  // @todo this test should use a mock, not the live 
+  const { search, read } = Client("https://r4.smarthealthit.org", {})
+
+  const searchQuery = await search("Patient");
+  const searchResponse = await searchQuery.json() as fhir4.Bundle;
+
+  expect(searchQuery.status).toEqual(200)
+  expect(searchResponse.resourceType).toEqual("Bundle")
+
+  const patientQuery = await read("Patient", "87a339d0-8cae-418e-89c7-8651e6aab3c6");
+  const patientResponse = await patientQuery.json() as fhir4.Patient
+
+  expect(patientQuery.status).toEqual(200)
+  expect(patientResponse.resourceType).toEqual("Patient")
+});
