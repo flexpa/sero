139644a5a793cced293fcdc23ed9903014108182
mseckykoebel <mseckykoebel@gmail.com>
2021-07-13 09:08:03 -0500

    added a patient view suggestion example shell, got ngrok to work with logica, and added some dummy patients

--- example/cds-hooks.ts
+++ example/cds-hooks.ts
@@ -1,13 +1,14 @@
-import { Config, Http, CDSHooks } from "../src"
+import { Config, Http, CDSHooks } from "../src";
 
 import appointmentBookExample from "./cds-hooks/appointment-book";
 import encounterDischargeExample from "./cds-hooks/encounter-discharge";
 import encounterStartExample from "./cds-hooks/encounter-start";
-import medicationPrescribeExample from "./cds-hooks/medication-prescribe"
+import medicationPrescribeExample from "./cds-hooks/medication-prescribe";
 import orderReviewExample from "./cds-hooks/order-review";
 import orderSelectExample from "./cds-hooks/order-select";
 import orderSignExample from "./cds-hooks/order-sign";
 import patientViewExample from "./cds-hooks/patient-view";
+import patientViewSuggestion from "./cds-hooks/patient-view-suggestion";
 
 const config: Config = {
   cdsHooks: {
@@ -19,7 +20,8 @@ const config: Config = {
       orderReviewExample,
       orderSelectExample,
       orderSignExample,
-      patientViewExample
+      patientViewExample,
+      patientViewSuggestion
     ],
     cors: true
   }
