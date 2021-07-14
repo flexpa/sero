0cbf07a8c357f85d03534d57121e80b2347fb329
Joshua Kelly <jdjkelly@gmail.com>
2021-07-14 13:10:14 -0400

    Completes the GoodRx example

--- example/goodrx/index.ts
+++ example/goodrx/index.ts
@@ -1,7 +1,11 @@
 import { start } from "../../src/http";
 import { Config, Http, CDSHooks } from "../../src"
 
+<<<<<<< HEAD
 import goodRxComparePriceService from "./good-rx-compare-price";
+=======
+import goodRxCompare from "./good-rx-compare-price-service";
+>>>>>>> ca99d70 (Completes the GoodRx example)
 
 const config: Config = {
   cdsHooks: {
