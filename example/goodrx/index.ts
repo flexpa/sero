8dc67b2fdc4645d0dc0b4d006b73d22fb1813282
Joshua Kelly <jdjkelly@gmail.com>
2021-07-15 09:59:19 -0400

    Require GOOD_RX_API_KEY to be set

--- example/goodrx/index.ts
+++ example/goodrx/index.ts
@@ -2,10 +2,14 @@ import { start } from "../../src/http";
 import { Config, Http, CDSHooks } from "../../src"
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import goodRxComparePriceService from "./good-rx-compare-price";
 =======
 import goodRxCompare from "./good-rx-compare-price-service";
 >>>>>>> ca99d70 (Completes the GoodRx example)
+=======
+import goodRxComparePriceService from "./good-rx-compare-price";
+>>>>>>> 7f1fa5a (Require GOOD_RX_API_KEY to be set)
 
 const config: Config = {
   cdsHooks: {
