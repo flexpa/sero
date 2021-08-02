1049900069ef45514c00749c98685af28474e251
mseckykoebel <mseckykoebel@gmail.com>
2021-08-02 13:53:21 -0500

    working on getting prefetch results for the calculation

--- example/cds-hooks-api-guide/src/index.js
+++ example/cds-hooks-api-guide/src/index.js
@@ -2,10 +2,11 @@ import { Http, CDSHooks, start } from "@sero.run/sero";
 
 import compareTimeService from "./current-time/current-time.js";
 import prefetchContext from "./prefetch-context/prefetch-context.js";
+import suggestionsLinksFhir from "./suggestions-links-fhir/suggestions-links-fhir.js";
 
 const config = {
   cdsHooks: {
-    services: [compareTimeService, prefetchContext],
+    services: [compareTimeService, prefetchContext, suggestionsLinksFhir],
     cors: true,
   },
 };
