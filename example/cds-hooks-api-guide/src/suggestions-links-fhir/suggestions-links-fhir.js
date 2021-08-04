beee3a886460028062053a96b8299b858e8c2d3d
mseckykoebel <mseckykoebel@gmail.com>
2021-08-04 10:49:04 -0500

    cleaned up suggestions-links service. Moving onto getting information from the patients EHR

--- example/cds-hooks-api-guide/src/suggestions-links-fhir/suggestions-links-fhir.js
+++ example/cds-hooks-api-guide/src/suggestions-links-fhir/suggestions-links-fhir.js
@@ -1,51 +1,38 @@
 import { Service, Card } from "@sero.run/sero";
-import { processPatientNames } from "./util.js";
 
-export default new Service(
-  {
-    id: "suggestions-links-fhir",
-    title: "Launch a SMART app from determining Reynolds Risk score",
-    hook: "patient-view",
-    description:
-      "A patient-view hook with prefetch values helping to determine Reynolds risk score. Launches a SMART app to help put results in context",
-    prefetch: {
-      patient: "Patient/{{context.patientId}}",
-      encounter: "Encounter?subject={{context.patientId}}&_sort=date",
-      hscrp: "Observation?code=http://loinc.org|30522-7",
-      cholesterolMassOverVolume: "Observation?code=http://loinc.org|2093-3",
-      hdl: "Observation?code=http://loinc.org|2085-9",
-      systolicBloodPressure: "Observation?code=http://loinc.org|8480-6",
-    },
+const options = {
+  id: "suggestions-links",
+  title: "patient view with Reynolds Risk Score assessment",
+  hook: "patient-view",
+  description:
+    "Service triggered on the patient-view hook that calculates Reynolds risk score, and makes a MedicationPrescribe suggestion based on the result. Also provides a link to a SMART app to help work with the result",
+  prefetch: {
+    patient: "Patient/{{context.patientId}}",
+    encounter: "Encounter?subject={{context.patientId}}&_sort=date",
+    hscrp: "Observation?code=http://loinc.org|30522-7",
+    cholesterolMassOverVolume: "Observation?code=http://loinc.org|2093-3",
+    hdl: "Observation?code=http://loinc.org|2085-9",
+    systolicBloodPressure: "Observation?code=http://loinc.org|8480-6",
   },
-  (request) => {
-    const data = request.prefetch;
-    const systolic = JSON.stringify(request.prefetch.systolicBloodPressure);
-    const patientNames = processPatientNames(data.patient);
-    return {
-      cards: [
-        // HSCRP data
-        new Card({
-          detail: `${systolic}`,
-          source: {
-            label: "Automate Medical, Inc.",
-            url: "https://www.automatemedical.com/",
-          },
-          summary: `HSCRP.`,
-          indicator: "info",
-        }),
-        // Name(s)
-        new Card({
-          detail: `This patient has ${patientNames.length} name${
-            patientNames.length <= 1 ? "" : "s"
-          } on record.`,
-          source: {
-            label: "Automate Medical, Inc.",
-            url: "https://www.automatemedical.com/",
-          },
-          summary: `Now seeing: ${patientNames[0].given} ${patientNames[0].family}.`,
-          indicator: "info",
-        }),
-      ],
-    };
-  }
-);
+};
+
+const handler = async (request) => {
+  const data = request.prefetch;
+  const systolic = JSON.stringify(request.prefetch.systolicBloodPressure);
+  return {
+    cards: [
+      // HSCRP data
+      new Card({
+        detail: `${systolic}`,
+        source: {
+          label: "Automate Medical, Inc.",
+          url: "https://www.automatemedical.com/",
+        },
+        summary: `HSCRP.`,
+        indicator: "info",
+      }),
+    ],
+  };
+};
+
+export default new Service(options, handler);
--- example/cds-hooks-api-guide/src/suggestions-links-fhir/util.js
+++ example/cds-hooks-api-guide/src/suggestions-links-fhir/util.js
@@ -1,13 +1,1 @@
-/**
- *
- * @param patient - a fhir patient
- * @returns an array of fhir human names
- * Return an array of patient names from the fhir patient bundle
- */
-export function processPatientNames(patient) {
-  const patientNames = [];
-  patient.name?.forEach((name) => {
-    patientNames.push(name);
-  });
-  return patientNames;
-}
+
