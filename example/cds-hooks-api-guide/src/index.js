2cccec0f28faaef942795e4b814c14455f16a79c
mseckykoebel <mseckykoebel@gmail.com>
2021-08-04 09:57:58 -0500

    removed the suggestions-links service for testing the other two services

--- example/cds-hooks-api-guide/src/index.js
+++ example/cds-hooks-api-guide/src/index.js
@@ -2,11 +2,10 @@ import { Http, CDSHooks, start } from "@sero.run/sero";
 
 import compareTimeService from "./current-time/current-time.js";
 import prefetchContext from "./prefetch-context/prefetch-context.js";
-import suggestionsLinksFhir from "./suggestions-links-fhir/suggestions-links-fhir.js";
 
 const config = {
   cdsHooks: {
-    services: [compareTimeService, prefetchContext, suggestionsLinksFhir],
+    services: [compareTimeService, prefetchContext],
     cors: true,
   },
 };
