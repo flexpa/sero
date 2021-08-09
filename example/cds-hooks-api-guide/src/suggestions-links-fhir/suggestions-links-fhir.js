8478bf547340fe194fcf9350171c1f3c20068ae0
mseckykoebel <mseckykoebel@gmail.com>
2021-08-09 10:09:13 -0500

    added the medicationRequest action template

--- example/cds-hooks-api-guide/src/suggestions-links-fhir/suggestions-links-fhir.js
+++ example/cds-hooks-api-guide/src/suggestions-links-fhir/suggestions-links-fhir.js
@@ -15,7 +15,8 @@ const options = {
   prefetch: {
     patient: "Patient/{{context.patientId}}",
     hscrp: "Observation?code=http://loinc.org|30522-7&_sort=date",
-    cholesterolMassOverVolume: "Observation?code=http://loinc.org|2093-3&&_sort=date",
+    cholesterolMassOverVolume:
+      "Observation?code=http://loinc.org|2093-3&&_sort=date",
     hdl: "Observation?code=http://loinc.org|2085-9&_sort=date",
     bloodPressure: "Observation?code=http://loinc.org|55284-4&_sort=date",
   },
@@ -43,9 +44,42 @@ const handler = async (request) => {
           {
             type: "create",
             description: "Create a prescription for Acetaminophen 250 MG",
+            fullUrl: "urn:uuid:3ba900b2-a795-40a0-8aae-1cfbb02e3ac1",
             resource: {
               resourceType: "MedicationRequest",
-              id: "medrx001",
+              id: "3ba900b2-a795-40a0-8aae-1cfbb02e3ac1",
+              status: "active",
+              intent: "order",
+              medicationCodeableConcept: {
+                coding: [
+                  {
+                    system: "http://www.nlm.nih.gov/research/umls/rxnorm",
+                    code: "429503",
+                    display: "Hydrochlorothiazide 12.5 MG",
+                  },
+                ],
+                text: "Hydrochlorothiazide 12.5 MG",
+              },
+              subject: {
+                reference: "urn:uuid:763b6101-133a-44bb-ac60-3c097d6c0ba1",
+              },
+              encounter: {
+                reference: "urn:uuid:09febec4-11a0-41b4-a5ef-5dabf8ffaf3e",
+              },
+              authoredOn: "1960-10-23T22:19:43-04:00",
+              requester: {
+                reference: "urn:uuid:0000016d-3a85-4cca-0000-00000000c5b2",
+                display: "Dr. Susan A Clark",
+              },
+              reasonReference: [
+                {
+                  reference: "urn:uuid:f810df60-74b0-4745-8fb5-cfe7e4c84a1e",
+                },
+              ],
+            },
+            request: {
+              method: "POST",
+              url: "MedicationRequest",
             },
           },
         ],
