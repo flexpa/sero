2520936140084766bfdb7f05f45e3169dedcbbd9
mseckykoebel <mseckykoebel@gmail.com>
2021-09-10 09:45:49 -0500

    basic version of the patient-view hook for the connectathon is working

--- example/connectathon-cds-hooks/src/patient-view/patient-view.js
+++ example/connectathon-cds-hooks/src/patient-view/patient-view.js
@@ -1,5 +1,9 @@
 import { Service, Card } from "@sero.run/sero";
-import { processAddresses, processPatientNames, processTelecom } from "./util.js";
+import {
+  processAddresses,
+  processPatientNames,
+  processTelecom,
+} from "./util.js";
 
 const options = {
   id: "sero-patient-view",
@@ -9,21 +13,18 @@ const options = {
     "Patient view hook for the cds hooks track for the September 2021 HL7 connectathon",
   prefetch: {
     patient: "Patient/{{context.patientId}}",
-    encounter: "Encounter?subject={{context.patientId}}&_sort=date",
   },
 };
 
 const handler = async (request) => {
   const data = request.prefetch;
   const patientNames = processPatientNames(data.patient);
-  const addresses = processAddresses(data.patient);
-  const telecom = processTelecom(data.patient);
   return {
     cards: [
       // Name(s)
       new Card({
         detail: `This patient has ${patientNames.length} name${
-          patientNames.length ? "s" : ""
+          patientNames.length > 1 ? "s" : ""
         } on record.`,
         source: {
           label: "Automate Medical, Inc.",
@@ -31,15 +32,13 @@ const handler = async (request) => {
         },
         summary: `Now seeing: ${patientNames[0].given} ${patientNames[0].family}.`,
         indicator: "info",
-      }),
-      // DOB
-      new Card({
-        source: {
-          label: "Automate Medical, Inc.",
-          url: "https://www.automatemedical.com/",
-        },
-        summary: `Date of birth: ${data.patient.birthDate}`,
-        indicator: "info",
+        links: [
+          {
+            label: "Launch CarePassport portal",
+            url: "https://carepassport.net/FHIR/launch.html",
+            type: "smart",
+          },
+        ],
       }),
       // Active
       new Card({
@@ -49,36 +48,6 @@ const handler = async (request) => {
           url: "https://www.automatemedical.com/",
         },
         summary: `Active`,
-        indicator: "info",
-      }),
-      // Address
-      new Card({
-        detail: `${addresses[0].line}, ${addresses[0].city}, ${addresses[0].state} ${addresses[0].postalCode}`,
-        source: {
-          label: "Automate Medical, Inc.",
-          url: "https://www.automatemedical.com/",
-        },
-        summary: `Current Address`,
-        indicator: "info",
-      }),
-      // Gender
-      new Card({
-        detail: `${data.patient.gender}`,
-        source: {
-          label: "Automate Medical, Inc.",
-          url: "https://www.automatemedical.com/",
-        },
-        summary: `Gender`,
-        indicator: "info",
-      }),
-      // Telecom (only pulls value of first element in array)
-      new Card({
-        detail: `${telecom[0].value}`,
-        source: {
-          label: "Automate Medical, Inc.",
-          url: "https://www.automatemedical.com/",
-        },
-        summary: `Contact`,
         indicator: "info",
       }),
     ],
--- example/connectathon-cds-hooks/src/patient-view/util.js
+++ example/connectathon-cds-hooks/src/patient-view/util.js
@@ -17,48 +17,6 @@ export function processPatientNames(patient) {
 
 /**
  *
- * @param {*} patient - a fhir4 patient
- * @returns an array of fhir addresses
- * Return an array of addresses from the fhir patient bundle
- */
-export function processAddresses(patient) {
-  const addresses = [];
-  patient.address?.forEach((address) => {
-    addresses.push(address);
-  });
-  return addresses;
-}
-
-/**
- *
- * @param {*} patient - a fhir4 patient
- * @returns an array of fhir contacts
- * Return an array of contacts from the fhir patient bundle
- */
-export function processContacts(patient) {
-  const contacts = [];
-  patient.contact?.forEach((address) => {
-    contacts.push(address);
-  });
-  return contacts;
-}
-
-/**
- *
- * @param {*} patient - a fhir4 patient
- * @returns an array of fhir contact points
- * Return an array of contact points (email and other things) from the fhir patient bundle
- */
-export function processTelecom(patient) {
-  const telecom = [];
-  patient.telecom?.forEach((address) => {
-    telecom.push(address);
-  });
-  return telecom;
-}
-
-/**
- *
  * @param {*} patient
  * @returns an array of identifiers for the patient
  */
