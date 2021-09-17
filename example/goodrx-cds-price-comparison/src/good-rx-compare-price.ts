aabe888fa044e2769169f0287383acb9dd368b16
mseckykoebel <mseckykoebel@gmail.com>
2021-09-17 09:22:24 -0500

    reverted back to the pre-connectathon changes

--- example/goodrx-cds-price-comparison/src/good-rx-compare-price.ts
+++ example/goodrx-cds-price-comparison/src/good-rx-compare-price.ts
@@ -2,14 +2,13 @@
 import { Service, Card, HookRequest, NoDecisionResponse } from "@sero.run/sero";
 import { Hooks } from "@sero.run/sero/cds-hooks/util";
 import { comparePrice } from "./api.js";
-import { randomUUID } from "crypto";
 
 const options = {
   id: "good-rx-comparison", // Used to define the HTTP routes for the Service
   title: "GoodRx Compare Price Service for MedicationRequest", // A narrative title used to generate UI config for Services by places like the LogicaSandbox
   hook: "order-select" as Hooks, // Identifies the "hook" context this service should be executed in (the hook determines what information will be sent)
   description:
-    "(Connectathon update) GoodRx's Compare Price API is used to provide drug cost estimates during the prescription order workflow",
+    "GoodRx's Compare Price API is used to provide drug cost estimates during the prescription order workflow",
 };
 
 const handler = async (request: HookRequest<any>) => {
@@ -22,7 +21,6 @@ const handler = async (request: HookRequest<any>) => {
    * request.context and it will be shaped like a FHIR Bundle.
    */
   const { draftOrders }: { draftOrders: fhir4.Bundle } = request.context;
-  const { patientId }: { patientId: string } = request.context;
   /**
    * The order-select hook *requires* that a Bundle of resources be provided
    * at draftOrders, but now that a MedicationRequest actually exists here. We
@@ -49,12 +47,6 @@ const handler = async (request: HookRequest<any>) => {
   const response = await comparePrice(medicationRequest);
 
   /**
-   * To make the medication request, we need to input a UUID
-   * Use a random one from crypto
-   */
-  const uuid = randomUUID();
-
-  /**
    * Create and return a Card that contains pricing information about the drug
    * from the GoodRx response. This example looks like:
    *
@@ -73,10 +65,6 @@ const handler = async (request: HookRequest<any>) => {
    * This would render a Card in the EHR with a link to the coupon actually
    * needed for the Patient to redeem the price.
    *
-   * This example also provides a suggestion response, where the user can
-   * select the option to prescribe the store-brand medication, or decline
-   * it for two different reasons. The "MedicationRequest" bundle is a sample
-   * bundle, and is not approved for production.
    */
   return {
     cards: [
@@ -96,68 +84,6 @@ const handler = async (request: HookRequest<any>) => {
         } savings)
           * Coupon: ${response.price_detail.url[0]}
         `,
-        suggestions: [
-          {
-            uuid: uuid,
-            label: `Perscribe ${response.generic.join(", ")} from ${
-              response.price_detail.pharmacy[0]
-            }`,
-            actions: [
-              {
-                type: "create",
-                description: `Create a prescription for ${response.generic.join(
-                  ", "
-                )}`,
-                resource: {
-                  resourceType: "MedicationRequest",
-                  status: "draft",
-                  intent: "proposal",
-                  medicationCodeableConcept: {
-                    coding: [
-                      {
-                        system: "urn:oid:2.16.840.1.113883.6.88",
-                        code: "313782",
-                        display: `${response.generic.join(", ")}`,
-                      },
-                    ],
-                    text: `${response.generic.join(", ")}`,
-                  },
-                  subject: {
-                    reference: `Patient/${patientId}`,
-                  },
-                  authoredOn: "2021-09-15T10:19:43-04:00",
-                  category: [
-                    {
-                      coding: [
-                        {
-                          code: "inpatient", // can be "inpatient" or "outpatient"
-                          system:
-                            "http://terminology.hl7.org/CodeSystem/medicationrequest-category", //This uses medicationrequest-category to utilize an existing FHIR mapping for inpatient/outpatient
-                          display: "Inpatient",
-                        },
-                      ],
-                    },
-                  ],
-                },
-              },
-            ],
-            isRecommended: true,
-          },
-        ],
-        overrideReasons: [
-          {
-            code: "patient-requested-brand",
-            system:
-              "http://terminology.cds-hooks.org/CodeSystem/OverrideReasons",
-            display: "Patient requested the brand name product",
-          },
-          {
-            code: "generic-drug-unavailable",
-            system:
-              "http://terminology.cds-hooks.org/CodeSystem/OverrideReasons",
-            display: "The generic medication was unavailable",
-          },
-        ],
       }),
     ],
   };
