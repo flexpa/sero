8d3639e436595b386bdedbf5dfa57ece73a98ff2
mseckykoebel <mseckykoebel@gmail.com>
2021-09-23 13:12:53 -0500

    added in the changes from 0.0.19 to the cds hooks example to see if it works (testing it out now)

--- example/cds-hooks-api-guide/src/index.js
+++ example/cds-hooks-api-guide/src/index.js
@@ -1,16 +1,14 @@
-import { Http, CDSHooks, start } from "@sero.run/sero";
+import { CDSHooks } from "@sero.run/sero";
+import Http from "fastify";
 
 import compareTimeService from "./current-time/current-time.js";
 import prefetchContext from "./prefetch-context/prefetch-context.js";
 import suggestionsLinksFhir from "./suggestions-links-fhir/suggestions-links-fhir.js";
 
 const config = {
-  cdsHooks: {
-    services: [compareTimeService, prefetchContext, suggestionsLinksFhir],
-    cors: true,
-  },
+  services: [compareTimeService, prefetchContext, suggestionsLinksFhir],
+  cors: true,
 };
 
-const http = Http(config);
-CDSHooks(config, http);
-start(http);
+const http = Http();
+http.register(CDSHooks, config);
