de4513d921bcb5277ff82593db62f266778e3541
Mason Secky-Koebel <33811742+mseckykoebel@users.noreply.github.com>
2021-10-07 13:48:59 -0500

    changing idp.ts to include a mandatory redirectHost property so that the tests do not fail

--- test/smart-auth/idp.ts
+++ test/smart-auth/idp.ts
@@ -3,6 +3,7 @@ import { SmartAuthProvider } from "../../src";
 const smartAuthProviderExample: SmartAuthProvider = {
   name: "idp",
   scope: ["launch"],
+  redirectHost: "http://localhost:3000/smart/provider/redirect",
   credentials: {
     client: {
       id: "123",
